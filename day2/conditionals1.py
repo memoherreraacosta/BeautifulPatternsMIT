15931e1baef8cc9ce6a7628a79ffa8ae59ca78a8
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-08-25 23:55:15 -0500

    Conditionals added

---
+++ day2/conditionals1.py
@@ -0,0 +1,20 @@
+"""
+
+Conditionals programming exercises
+
+Numero mas grande
+"""
+
+"""
+If statements
+
+if, else, elif, continue, break
+"""
+
+x = int(input("Escoge un numero 'x': "))
+y = int(input("Escoge un numero 'y': "))
+
+if x > y:
+    print("El numero x: {0} es mayor".format(x))
+else:
+    print("El numero y: {0} es mayor".format(y))
---
+++ day2/conditionals3.py
@@ -0,0 +1,26 @@
+"""
+
+Conditionals programming exercises
+
+Calificaciones
+"""
+
+"""
+If statements
+
+if, else, elif, continue, break
+"""
+
+x = int(input("Calificacion alumno 'x': "))
+calificacion = "Calificacion muy alta"
+
+if 100 >= x:
+    calificacion = "A"
+    if 90 >= x:
+        calificacion = "B"
+        if 80 >= x:
+            calificacion = "C"
+            if 70 > x:
+                calificacion = "Numero Negativo" if x < 0 else "F"
+
+print("La calificacion alfabetica de '{0}' es {1} ".format(x, calificacion))
