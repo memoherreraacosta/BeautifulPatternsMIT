8934b4f85da61982f99b9b42a5782bd99ec08138
Nicole Carrillo <42300492+NicoleCarrillo@users.noreply.github.com>
2020-09-03 12:34:55 -0400

    Update Lista_Elementos.py

--- day3/ListaElementos/Lista_Elementos.py
+++ day3/ListaElementos/Lista_Elementos.py
@@ -2,12 +2,16 @@ lista1=["HOLA", "Mundo"]
 lista2=[2,0,2,0]
 lista3=[1,2,3,"probando...","accion!"]
 
-def imprimir(lista):
-    for x in range(len(lista)):
-        print(lista[x],end="  ")
+    
+for x in range(len(lista1)):
+    print(lista1[x],end="  ")
 
-imprimir(lista1)
 print(" ")
-imprimir(lista2)
+
+for x in range(len(lista2)):
+    print(lista2[x],end="  ")
+    
 print(" ")
-imprimir(lista3)
+
+for x in range(len(lista3)):
+    print(lista3[x],end="  ")
