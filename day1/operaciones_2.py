f138a76a474c1845fa6f9b71bb1ad85a9c5ab6f5
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2020-09-03 03:06:12 -0500

    Problemas dia 1 python
    Signed-off-by: nataly-8h <54066974+nataly-8h@users.noreply.github.com>

--- day1/operaciones_2.py
+++ day1/operaciones_2.py
@@ -17,12 +17,3 @@ print ("Division enteros: " + str(int(n2 // n1)))
 print ("Módulo residuo: " + str(n2 % n1))
 print("Division y suma: " + str(n1 * n2 + n1))
 print("Division y suma con paréntesis: " + str(n1 * (n2 + n1)))
-
-"""
-Extra: utiliza la librería de Math
-"""
-print("\nPotencia pero cool: " + str(math.pow(n1, n2)))
-print("Primer número multiplicado por pi: " + str(n1 * math.pi))
-print("Raiz cuadrada: " + str(math.sqrt(n2)))
-print("Ceil: " + str(math.ceil(n1)))
-print("Floor: " + str(math.floor(n1)))
