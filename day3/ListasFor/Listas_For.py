aceb462479762b6eb02159c2333d879ee30de03f
Nicole Carrillo <42300492+NicoleCarrillo@users.noreply.github.com>
2020-09-03 12:39:06 -0400

    Update Listas_For.py

--- day3/ListasFor/Listas_For.py
+++ day3/ListasFor/Listas_For.py
@@ -1,8 +1,6 @@
 preguntas=['nombre', 'objetivo', 'edad']
 respuestas=['Leonardo', 'aprender Python', '18']
 
-def imprimirValores(preguntas,respuestas):
-    for preguntas, respuestas in zip(preguntas, respuestas):
-     print ("¿Cual es tu", preguntas, "?"," la respuesta es: ",respuestas)
 
-imprimirValores(preguntas,respuestas)
+for preguntas, respuestas in zip(preguntas, respuestas):
+    print ("¿Cual es tu", preguntas, "?"," la respuesta es: ",respuestas)
